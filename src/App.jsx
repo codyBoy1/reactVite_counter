ea9430b469e9ce8dac46ddc2f49034075920d9c8
cody <rv502887@gmail.com>
2024-03-29 09:19:54 +0530

    counter app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-
+import {useState} from 'react'
 
 function App() {
-
+//let counter =0;// this doesnot work in UI
+let [counter , setCounter ] = useState(0)
+const addValue = () => {
+  if(counter<20){
+    counter = counter  +1 
+  setCounter(counter)
+  }else{
+    alert("counter can't exceed to 20")
+  }
+}
+const subValue = () => {
+  
+  if(counter>0){
+  counter = counter -1 
+  setCounter(counter)//to update value 
+    
+  }else{
+    alert("counter can't be less than 0")
+  }
+}
   return (
     <>
       <h3> counter app </h3>
+      <p>count value : {counter}</p>
+      <button onClick={addValue}>addValue</button><br/>
+      <button onClick={subValue}>subValue</button>
     </>
   )
 }
